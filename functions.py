b8c9f59ceffbbc0d890dfde8631115fd2f0914a2
Nikita <you@example.com>
2025-05-26 01:16:18 +0530

    Add initial implementation of To-Do app with file handling and Streamlit integration
    Signed-off-by: Nikita <you@example.com>

---
+++ functions.py
@@ -0,0 +1,22 @@
+import os
+
+FILEPATH = r"C:\Users\ad17730\OneDrive - Lumen\Python\Python udemy\Projects_local\To-Do App\todos.txt"
+
+def check_filepath():
+    if not os.path.exists(FILEPATH):
+        with open(FILEPATH, 'w') as file:
+            pass
+
+def get_todos(filepath=FILEPATH):
+    """Read a text file and return the list of to-do items."""
+    with open(filepath,'r') as file:
+            todos_local = file.readlines()
+    return todos_local
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """Write the to-do items list in the text file."""
+    with open(filepath,'w') as file:
+            file.writelines(todos_arg)
+
+if __name__ == "__main__":
+    print(get_todos())  
---
+++ web.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import functions as fn
+
+todos = fn.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    fn.write_todos(todos)
+    st.session_state["new_todo"] =""
+
+st.title("To-Do App")
+st.write("You can manage your tasks here.")
+
+for index ,todo in enumerate(todos):
+    checkbox = st.checkbox(todo.strip(), key=todo)
+    if checkbox:
+        todos.pop(index)
+        fn.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+st.text_input(label = "Add new task:",key="new_todo",
+              placeholder="Enter a new task here..."
+              ,on_change=add_todo)
+
